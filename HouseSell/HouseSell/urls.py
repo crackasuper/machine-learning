fc8a81ae9dfdcdbec553fc546641334fb4fc142a
crackasuper <crackabad859@gmail.com>
2024-03-29 09:56:02 +0300

    house sell prediction using linear regression and djang

--- HouseSell/HouseSell/urls.py
+++ HouseSell/HouseSell/urls.py
@@ -19,5 +19,7 @@ from django.urls import path
 from HousePrice import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.home, name='home')
+    path("", views.home, name='home'),
+    path('predict', views.predict, name='predict'),
+    path('predict/result', views.result, name='result')
 ]
